ee698a4f70c8f39a9fc66409b62c9895c11dc2fd
nobody <nobody@163d3ccd-8fcc-98f7-87d1-f3bf96b86f93>
2009-08-02 12:09:29 +0000

    Debug for get buddy icon

--- get_buddyicon.c
+++ get_buddyicon.c
@@ -21,15 +21,20 @@ juice_GET_buddyicon(gchar *buddyname, gchar *proto_id, gchar *proto_username)
 	account = purple_accounts_find(proto_username, proto_id);
 	if (account == NULL)
 		return g_strdup("");
+	purple_debug_info("purple_juice", "account not null\n");
 	
 	icon = purple_buddy_icons_find(account, buddyname);
 	if (icon == NULL)
 		return g_strdup("");
+	purple_debug_info("purple_juice", "icon not null\n");
 	
 	buddy_icon_data = purple_buddy_icon_get_data(icon, &buddy_icon_len);
+	purple_debug_info("purple_juice", "buddy_icon_data address: %d \n", buddy_icon_data);
 	buddy_icon_data = g_strndup(buddy_icon_data, buddy_icon_len);
+	purple_debug_info("purple_juice", "buddy_icon_data address2: %d \n", buddy_icon_data);
 	
 	purple_buddy_icon_unref(icon);
+	purple_debug_info("purple_juice", "buddy_icon_data address3: %d \n", buddy_icon_data);
 	
 	return buddy_icon_data;
 }
