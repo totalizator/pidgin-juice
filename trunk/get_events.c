fe84d3fec86281a12ffe104bb3fadb8268c17395
nobody <nobody@163d3ccd-8fcc-98f7-87d1-f3bf96b86f93>
2009-08-08 00:14:25 +0000

    Connected signals for IM/typing events

---
+++ trunk/get_events.c
@@ -0,0 +1,74 @@
+/*
+ *  get_events.c
+ *  pidgin_juice
+ *
+ *  Created by MyMacSpace on 8/08/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+static void
+received_im_msg_cb(PurpleAccount *account, char *sender, char *message,
+					PurpleConversation *conv, PurpleMessageFlags flags,
+					gpointer user_data)
+
+{
+
+
+}
+
+static void
+buddy_typing_cb(PurpleAccount *account, const char *name, gpointer user_data)
+{
+	
+}
+static void 
+buddy_typing_stopped_cb(PurpleAccount *account, const char *name, gpointer user_data)
+{
+	
+}
+
+struct _ConnectedSignals {
+	gulong received_im_signal;
+	gulong buddy_typing_signal;
+	gulong buddy_typing_stoppped_signal;
+};
+
+static struct _ConnectedSignals ConnectedSignals = NULL;
+
+static void
+connect_to_signals()
+{
+    void *conv_handle = purple_conversations_get_handle();
+	
+	if (ConnectedSignals == NULL)
+		ConnectedSignals = g_new0(struct _ConnectedSignals, 1);
+	else
+		return;
+	
+	purple_signal_connect(conv_handle, "received-im-msg",
+						  ConnectedSignals, PURPLE_CALLBACK(received_im_msg_cb), NULL);
+    purple_signal_connect(conv_handle, "buddy-typing",
+						  ConnectedSignals, PURPLE_CALLBACK(buddy_typing_cb), NULL);
+	purple_signal_connect(conv_handle, "buddy-typing-stopped",
+						  ConnectedSignals, PURPLE_CALLBACK(buddy_typing_stopped_cb), NULL);
+
+	//add a timeout here to disconnect
+}
+
+static gboolean
+disconnect_signals()
+{
+	purple_signal_disconnect_by_handle(ConnectedSignals);
+	g_free(ConnectedSignals);
+	ConnectedSignals = NULL;
+	
+	return FALSE; //return false so that the event doesn't get fired twice
+}
+
+static void
+juice_GET_events(gsize *length)
+{
+	//this funciton should block until there's an event, or until a minute is up
+	
+}
