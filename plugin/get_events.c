0bff6eef2d3fd2c945f6b9bb2ffbf87c3623701b
nobody <nobody@163d3ccd-8fcc-98f7-87d1-f3bf96b86f93>
2009-08-09 10:09:55 +0000

    Fix events, handle incomming messages

--- plugin/get_events.c
+++ plugin/get_events.c
@@ -11,10 +11,59 @@ static void
 received_im_msg_cb(PurpleAccount *account, char *sender, char *message,
 				   PurpleConversation *conv, PurpleMessageFlags flags,
 				   gpointer user_data)
-
 {
+	gchar *output;
+	JsonObject *json_message;
+	JsonNode *json_message_node;
+	JsonNode *sender_node, *message_node, *timestamp_node, *type_node;
+	JsonNode *proto_id_node, *proto_username_node;
 	
-	g_queue_push_tail(&message_queue, g_strdup(""));
+	json_message = json_object_new();
+	
+	sender_node = json_node_new(JSON_NODE_VALUE);
+	escaped = g_strescape(sender, "");
+	json_node_set_string(sender_node, escaped);
+	g_free(escaped);
+	json_object_add_member(json_message, "sender", sender_node);
+	
+	message_node = json_node_new(JSON_NODE_VALUE);
+	escaped = g_strescape(message, "");
+	json_node_set_string(message_node, escaped);
+	g_free(escaped);
+	json_object_add_member(json_message, "message", message_node);
+	
+	type_node = json_node_new(JSON_NODE_VALUE);
+	json_node_set_string(type_node,
+						 (flags & PURPLE_MESSAGE_RECV?"received":"sent"));
+	json_object_add_member(json_message, "type", type_node);
+	
+	timestamp_node = json_node_new(JSON_NODE_VALUE);
+	json_node_set_int(timestamp_node, 
+					  time(NULL));
+	json_object_add_member(json_message, "timestamp", timestamp_node);
+	
+	proto_id_node = json_node_new(JSON_NODE_VALUE);
+	escaped = g_strescape(purple_account_get_protocol_id(account), "");
+	json_node_set_string(proto_id_node, escaped);
+	g_free(escaped);
+	json_object_add_member(json_message, "proto_id", proto_id_node);
+	
+	proto_username_node = json_node_new(JSON_NODE_VALUE);
+	escaped = g_strescape(purple_account_get_username(account), "");
+	json_node_set_string(proto_username_node, escaped);
+	g_free(escaped);
+	json_object_add_member(json_message, "proto_username", proto_username_node);
+	
+	json_message_node = json_node_new(JSON_NODE_OBJECT);
+	json_node_take_object(json_message_node, json_message);
+	
+	generator = json_generator_new();
+	json_generator_set_root(generator, json_message_node);
+	
+	output = json_generator_to_data(generator, NULL);	
+	json_node_free(json_message_node);	
+	
+	g_queue_push_tail(&message_queue, output);
 }
 
 static void
@@ -35,6 +84,7 @@ buddy_typing_stopped_cb(PurpleAccount *account, const char *name, gpointer user_
 #endif
 
 static GQueue message_queue = G_QUEUE_INIT;
+static GHashTable *channels = NULL; 
 
 struct _ConnectedSignals {
 	guint disconnect_timer;
@@ -43,8 +93,8 @@ struct _ConnectedSignals {
 	gulong buddy_typing_signal;
 	gulong buddy_typing_stopped_signal;
 };
-
 static struct _ConnectedSignals ConnectedSignals = NULL;
+
 
 static void
 connect_to_signals()
@@ -110,6 +160,8 @@ disconnect_signals_cb(gpointer data)
 		//clear the queue
 	}
 	
+	current_seq = 0;
+	
 	return FALSE; //return false so that the event doesn't get fired twice
 }
 
@@ -120,6 +172,15 @@ write_to_client(GIOChannel *channel)
 	gchar *headers = NULL;
 	gchar *next_event;
 	gboolean first = TRUE;
+	guint timeout;
+	
+	if (channels != NULL)
+	{
+		timeout = GPOINTER_TO_INT(g_hash_table_lookup(channels, channel));
+		if (timeout)
+			purple_timeout_remove(timeout);
+		g_hash_table_steal(channels, channel);
+	}
 	
 	returnstring = g_string_new("{ \"events\" : [ ");
 	
@@ -162,9 +223,40 @@ static guint current_seq = 0;
 
 static void
 juice_GET_events(GIOChannel *channel)
-{	
+{
+	gchar *body = NULL;
+	gchar *headers = NULL;
+	guint timeout;
+	
 	connect_to_signals();
 	
+	if (FALSE)
+	{ //todo, change this to check $_GET['sequence'] with the current_seq
+		body = g_strdup_printf("{ \"events\" : [ { \"type\":\"new_seq\", \"seq\":%d } ] }",
+							   current_seq);
+		
+		headers = g_strdup_printf("HTTP/1.0 200 OK\r\n"
+								  "Content-type: application/json\r\n"
+								  "Content-length: %d\r\n\r\n", strlen(body));
+		
+		write_data(channel, G_IO_OUT, headers, strlen(headers))
+		write_data(channel, G_IO_OUT, body, strlen(body)+1);
+		
+		g_free(headers);
+		g_free(body);
+		return;
+	}
+	
 	if (!g_queue_is_empty())
+	{
 		write_to_client(channel);
+		return;
+	}
+	
+	if (channels == NULL)
+		channels = g_hash_table_new_full(NULL, NULL, NULL, NULL);
+	
+	//Otherwise, store up the channel
+	timeout = purple_timeout_add_seconds(60, (GSourceFunc)write_to_client, channel);
+	g_hash_table_insert(channel, GINT_TO_POINTER(timeout));
 }
